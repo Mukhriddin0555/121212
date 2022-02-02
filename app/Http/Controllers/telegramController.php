b548db6f5354fa2b1c8b74ce4e9eb0fe0333450b
Mukhriddin0555 <romantik4350555@gmail.com>
2022-02-02 14:35:17 +0500

    add telerambot

--- app/Http/Controllers/telegramController.php
+++ app/Http/Controllers/telegramController.php
@@ -14,21 +14,21 @@ class telegramController extends Controller
     {
         $chat_id = $request['message']['chat']['id'];
         $text = $request['message']['text'];
-        if(strlen($text) >= 3){
+        if(strlen($text) >= 4){
             $search =  '%'.$text.'%'; //str_replace("*", "%", $search1);
             $get = HowToModel::where('zavod_sn', 'LIKE', "$search");
-            $count = $get->get();
+            $count = $get->getOrFail();
             if($count){
                 $data1 = HowToModel::where('zavod_sn', 'LIKE', "$search")->get();
-            $data = (string)view('telegramsupport.howtomodel',['models' => $data1]);
-            $telegram->sendMessage($chat_id, $data);
+                $data = (string)view('telegramsupport.howtomodel',['models' => $data1]);
+                $telegram->sendMessage($chat_id, $data);
             }else{
                 $data = 'В базе данных со значением'.$text.' нечего не найдено';
                 $telegram->sendMessage($chat_id, $data);
             }
             
         }else{
-            $data = 'Вы должны отправить не менне 3 символа';
+            $data = 'Вы должны отправить не менне 4 символа';
             $telegram->sendMessage($chat_id, $data);
         }
         
