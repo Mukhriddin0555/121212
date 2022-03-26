897d0a840d16759540b96d9d8b74591fbddca8c5
Mukhriddin0555 <romantik4350555@gmail.com>
2022-03-26 16:37:56 +0500

    import new version

--- app/Imports/me2n_Import.php
+++ app/Imports/me2n_Import.php
@@ -3,12 +3,13 @@
 namespace App\Imports;
 
 use App\Models\waiting;
+use App\Models\sparepart;
 use App\Models\me2nImport;
 use PhpParser\Node\Expr\New_;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use function PHPUnit\Framework\isNull;
 
+use function PHPUnit\Framework\isNull;
 use Maatwebsite\Excel\Concerns\ToModel;
 use App\Models\me2nImport as ModelsMe2nImport;
 
@@ -22,15 +23,17 @@ class me2n_Import implements ToModel
         
         if($rows[0] != null && $rows[1] != null && $rows[2] != null && $rows[3] != null && $rows[4] != null && $rows[5] != null ){
             if($rows[5] == 'net'){
+                $sparepart_id = sparepart::firstOrCreate(['sap_kod' => $rows[1]],['name' => 'Не найден']);
+                $sap_id = $sparepart_id->id;
+                
                 $me2n = DB::table('me2n_imports')
                 ->where('sap', $rows[1])
                 ->where('postupleniye', $rows[4])
                 ->count();
-                
                 $order = DB::table('waitings')
                 ->where('status_id', 1)
                 ->where('crm_id', $rows[0])
-                ->where('sap_kod', $rows[1])
+                ->where('sparepart_id', $sap_id)
                 ->first();
     
                     
@@ -40,7 +43,7 @@ class me2n_Import implements ToModel
                 $order = DB::table('waitings')
                 ->where('status_id', 1)
                 ->where('crm_id', $rows[0])
-                ->where('sap_kod', $rows[1])
+                ->where('sap_kod', $sap_id)
                 ->where('how', $rows[2])
                 ->update(["status_id" => 3]);
 
@@ -53,25 +56,25 @@ class me2n_Import implements ToModel
                 }
                 if($me2n == 0){
                     if($rows[0] == 'rsp'){
-                        $warehouse = DB::table('waitings')
+                        $waitings = DB::table('waitings')
                         ->join('warehouses', 'waitings.warehouse_id', '=', 'warehouses.id')
-                        ->select('waitings.id', 'warehouses.Kod', 'waitings.sap_kod', 'waitings.status_id', 'waitings.how' )
+                        ->select('waitings.id', 'warehouses.Kod', 'waitings.sparepart_id', 'waitings.status_id', 'waitings.how' )
                         ->where('waitings.status_id', 1)
                         ->where('warehouses.Kod', $rows[3])
-                        ->where('sap_kod', $rows[1])
+                        ->where('sap_kod', $sap_id)
                         ->get();
                         
                         
-                        if(!empty($warehouse)){
+                        if(!empty($waitings)){
                             
                             $count = $rows[2];
                             
-                            foreach ($warehouse as $house){
-                                if($count >= $house->how){
-                                    $wait = waiting::find($house->id);
-                                    $wait->status_id = 3;
-                                    $wait->save();
-                                    $count -= $wait->how;
+                            foreach ($waitings as $wait){
+                                if($count >= $wait->how){
+                                    $waiting = waiting::find($house->id);
+                                    $waiting->status_id = 3;
+                                    $waiting->save();
+                                    $count -= $waiting->how;
 
                                     $me2ncreate = new me2nImport();
                                     $me2ncreate->sap = $rows[1];
