ae03f74a522c3d02fd875c0f924cc11fab7bb94d
Mukhriddin0555 <romantik4350555@gmail.com>
2022-05-07 18:40:11 +0500

    sss

--- routes/web.php
+++ routes/web.php
@@ -69,7 +69,7 @@ Route::get('/clear', function () {
     Artisan::call('config:clear');
     Artisan::call('view:clear');
     Artisan::call('route:clear');
-    return "Кэш очищено";
+    return "Кэш очищенный";
 });
 
 require __DIR__.'/auth.php';
